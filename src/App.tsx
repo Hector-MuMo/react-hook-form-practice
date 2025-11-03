0bb4e6cb2b4901ee5abd2638ab0d49e21910e823
Hector Muñoz <hectormm.wrk@gmail.com>
2025-11-03 14:54:51 -0600

    normal form vs react hook form - basico

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { FoodDeliveryForm } from './FoodDeliveryForm'
+
+function App() {
+
+
+  return (
+    <>
+      <FoodDeliveryForm />
+    </>
+  )
+}
+
+export default App
