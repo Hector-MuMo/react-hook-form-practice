de615935f8a35041242455e2798c9b4b954e6955
Hector Muñoz <hectormm.wrk@gmail.com>
2025-11-05 10:50:06 -0600

    the formState properties and how works

--- src/pages/FoodDelivery/FoodDeliveryForm.tsx
+++ src/pages/FoodDelivery/FoodDeliveryForm.tsx
@@ -37,7 +37,25 @@ export const FoodDeliveryForm = () => {
       },
     });
 
-  const { handleSubmit } = methods;
+  const { handleSubmit, formState } = methods;
+  //IMPORTANT: When we destructure formState, we create a subscription to the properties we destructure 
+  // and any change in any property will cause a re-render.
+  // For example isDirty wil re-render the component on every change of the boolean state.
+  /*
+  isDirty: a boolean indicating whether any of the form fields have been modified from their initial values.
+  isValid: a boolean indicating whether the entire form is valid based on the defined validation rules.
+  isValidating: a boolean indicating whether the form is currently undergoing validation.
+  errors: an object containing validation errors for each form field, if any exist.
+  dirtyFields: an object indicating which specific fields have been modified.
+  touchedFields: an object indicating which specific fields have been interacted with (focused and then blurred).
+  defaultValues: an object containing the initial values of the form fields.
+  isSubmitting: a boolean indicating whether the form is currently being submitted.
+  isSubmitted: a boolean indicating whether the form has been submitted at least once.
+  isSubmitSuccessful: a boolean indicating whether the last form submission was successful.
+  submitCount: a number indicating how many times the form has been submitted.
+  isLoading: a boolean indicating whether the form is in a loading state, typically used for async operations.
+  */
+  const { isDirty } = formState;
 
   const onSubmit = (formData: FoodDeliveryFormType) => {
     console.log("form data", formData);
